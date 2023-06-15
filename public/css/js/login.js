efa10a5ff9dc0c47c6ed4c50d6212cb6cae2a32c
DanialDevelops <danialchou101@gmail.com>
2023-06-15 18:43:11 -0400

    added some js code for login

---
+++ public/css/js/login.js
@@ -0,0 +1,8 @@
+const loginHandler = async (event) =>{
+    event.preventDefault();
+
+    const password = document.querySelector("#password-login").value.trim()
+    const email = document.querySelector("#email-login").value.trim()
+
+    
+}
