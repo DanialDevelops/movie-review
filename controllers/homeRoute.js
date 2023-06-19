11f31cb089e2dd2c7c2130476186220b9b37dfc8
Peter Park <peterjpark@pm.me>
2023-06-19 13:35:46 -0400

    Update HTML routes

---
+++ controllers/homeRoute.js
@@ -0,0 +1,11 @@
+// const router = require('express').Router();
+// const { Review, User, Movie } = require('../models');
+// const withAuth = require('../utils/auth');
+
+// router.get('/', async (req, res) => {
+//     try {
+//         const
+//     }
+// }
+
+// module.exports = router;
