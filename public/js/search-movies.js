eba118bd23f615089eced1e01713ac7ab6ce6e2c
Peter Park <peterjpark@pm.me>
2023-06-19 14:21:36 -0400

    Make movie results clickable

--- public/js/search-movies.js
+++ public/js/search-movies.js
@@ -18,18 +18,21 @@ searchForm.addEventListener('submit', async (e) => {
   // Display search results.
   movies.forEach((movie) => {
     const movieCard = document.createElement('div');
-
+    movieCard.setAttribute('data-id', movie.id);
     movieCard.classList.add('card', 'col-3', 'm-1');
     movieCard.innerHTML = `
     <img class='card-img-top' src='${movie.imageUrl}' alt='${movie.title}' />
         <div class='card-body'>
             <h5 class='card-title'>${movie.title}</h5>
-            <p class="card-text">Average Rating: ${movie.avgRating.toFixed(
-              1
-            )}</p>
+            <p class="card-text">Average Rating: ${movie.avgRating}</p>
             <p class="card-text">Reviews: ${movie.reviewsCount}</p>
         </div>
         `;
     searchResults.appendChild(movieCard);
+    movieCard.addEventListener('click', async (e) => {
+      e.preventDefault();
+      const movieId = e.currentTarget.getAttribute('data-id');
+      window.location.replace(`/movie/${movieId}`);
+    });
   });
 });
