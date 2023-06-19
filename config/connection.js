43229e4dd1a59d2e45702a405fcc9d2c901c5ccd
Peter Park <peterjpark@pm.me>
2023-06-19 19:18:19 -0400

    Fix connection

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Create a new Sequelize instance, using environment variables for the parameters
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  }
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 // Test database connection
 sequelize
