c0cf106cf6c6cc0d2d688441c8797d7330f9e7d6
Mohammadmahdi Moradzadeh <126528880+Mahdi-Moradzadeh@users.noreply.github.com>
2023-06-19 23:30:37 -0400

    Cleaned Profile hbr, Added WL hbr modified srch

--- public/js/search-movies.js
+++ public/js/search-movies.js
@@ -18,8 +18,10 @@ searchForm.addEventListener('submit', async (e) => {
   // Display search results.
   movies.forEach((movie) => {
     const movieCard = document.createElement('div');
+    const watchlistButton = document.createElement('button');
     movieCard.setAttribute('data-id', movie.id);
     movieCard.classList.add('card', 'col-3', 'm-1');
+    watchlistButton.classList.add('btn', 'btn-primary', 'm-1');
 
     let cardContent = `
       <img class='card-img-top' src='${movie.imageUrl}' alt='${movie.title}' />
@@ -33,8 +35,24 @@ searchForm.addEventListener('submit', async (e) => {
       </div>
       `;
     movieCard.innerHTML = cardContent;
+    // for(let i = 0; i < watchlistDB.length; i++){
+    //   if(movie.id === watchlistDB[i].movie_id){
+    //     watchlistButton.textContent = 'Remove from Watchlist';
+    //     return;
+    //   }
+    //   watchlistButton.textContent = 'Add to Watchlist';
+    // }
 
+    watchlistButton.textContent = 'Add to Watchlist';
+    
+    movieCard.appendChild(watchlistButton);
     searchResults.appendChild(movieCard);
+    watchlistButton.addEventListener('click', async (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    
+
+    });
     movieCard.addEventListener('click', async (e) => {
       e.preventDefault();
       // Put selected movie in sessionStorage so we don't have to make another API call.
@@ -60,3 +78,9 @@ async function addMovieToSession(movie) {
     console.error('Failed to add movie to session.');
   }
 }
+
+// on the page load send ajax request to get all watchlist from the database
+// this request will check if the user is logged in or not
+// if logged in then send the data of watchlist as json (API route)
+// after receiving the data, save in local storage
+// after fetching movies api data then compared it to local storage data
